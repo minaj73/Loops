e4d4d81a81f1ee093a0dd18699a9718b9eaae587
minaj73 <93118731+minaj73@users.noreply.github.com>
2021-11-06 23:41:57 +0530

    Add files via upload

---
+++ loop statement _ do while.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int i=1;
+    do
+    {
+        printf("\n India");
+        i++;
+    }
+    while(i<=5);
+        printf("\n Thanks");
+    getch();
+    return 0;
+}
---
+++ loop statement _ for.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+
+{
+int i;
+    for(i=1;i<=5;i++)
+    {
+        printf("\n India");
+    }
+        printf("\n Thanks");
+    getch();
+    return 0;
+}
